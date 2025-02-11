f06a9664a377dcdc1e687b8284bf29de64c6495a
medlaq777 <m.laqsoumi@gmail.com>
2025-02-12 00:30:39 +0100

    get the total users on the admin dashboard

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+
 use App\Core\Controller;
 class DashboardController extends Controller
 {
@@ -10,21 +12,36 @@ class DashboardController extends Controller
         parent::__construct();
     }
 
+
+
+    public function totalUsers()
+{
+    $users = User::findAll();
+    $totalUsers = count($users);
+    
+    return $totalUsers;
+}
+
+
+
     public function dashboard()
     {
         $case = $_SESSION['user_role'];
-        error_reporting(E_ALL);
-    ini_set('display_errors', 1);
-        
+
         switch ($case) {
             case 'Admin':
-                $this->render('Admin/index');
+                $dashboard = [
+                    'totalUsers' => $this->totalUsers(),
+                    'totalEvents' => 0, 
+                    'pendingEvents' => [] 
+                ];
+                
+                $this->render('Admin/index', ['dashboard' => $dashboard]);
                 break;
             case 'Organisateur':
                 $this->render('Organisateur/index');
                 break;
             default:
-                var_dump('Entering default case'); // Add this line
                 $this->render('Participant/index');
                 break;
         }
--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@
 
 use App\Controllers\AuthController;
 use App\Controllers\DashboardController;
+use App\Controllers\AdminController;
 
 // Home route
 $router->get('/', [AuthController::class, 'login']);
@@ -11,3 +12,6 @@ $router->get('/register', [AuthController::class, 'register']);
 $router->post('/register', [AuthController::class, 'register']);
 $router->get('/logout', [AuthController::class, 'logout']);
 $router->get('/dashboard', [DashboardController::class, 'dashboard']);
+$router->get('/totalUsers', [AdminController::class, 'totalUsers']);
+// $router->get('/admin/events', [AdminController::class, 'totalEvents']);
+// $router->get('/admin/events/pending', [AdminController::class, 'pendingEvents']);

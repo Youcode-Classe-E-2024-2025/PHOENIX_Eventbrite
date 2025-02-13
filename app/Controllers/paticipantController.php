088648e81bd823fa0a9ad02e01cb3da6198d8f10
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2025-02-13 12:48:54 +0000

    commit 4 commit

--- app/Controllers/paticipantController.php
+++ app/Controllers/paticipantController.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace App\Controllers;
 
 use App\Core\Controller;
@@ -6,69 +7,65 @@ use App\Models\Event;
 use App\Models\Reservation;
 use PHPUnit\Framework\Constraint\Count;
 
-    class PaticipantController extends Controller{
-
-        public function __construct(){
-            parent::__construct();
-        }
-
-        public function  findAllEvent(){
-            $events = Event::findAllEvent();
-        return $events ;
-        }
-        public function accederEventReserver(){
-        
-        }
-        public function Events() {
-            $events = $this->findAllEvent(); 
-            $this->render('Participant/events', ['events' => $events]);
-        }
-
-        public function getPaginationEvent($page)
-        {
-            $limit = 4;
-            if (!is_numeric($page) || $page < 1) {
-                $page = 1;
-            }
-            $offset = ($page - 1) * $limit;
-            $events = Event::getPaginationEvent($limit, $offset);
-            $totalEvents = count($this->findAllEvent());
-            $totalPages = ceil($totalEvents / $limit);
-         return json_encode([
-                'events' => $events,
-                'currentPage' => (int)$page,
-                'totalPages' => $totalPages,
-                'totalEvents' => $totalEvents
-            ]);
-        }
-        
-        public function EventsPagination($page) {
-            $events = $this->getPaginationEvent($page); 
-            $page = $events['currentPage'];
-            $totalPages = $events['totalEvents'];
-            $totalEvents = $events['totalEvents'];
-            var_dump($page);
-            if (!$events['events']){
-                throw new \Exception('Event not found', 404);
-            }
-            $this->render('Participant/events', ['events' => $events['events'] , 'page'=> $page , 'totalEvents'=>$totalPages , 'totalEvents' =>$totalEvents]);
-        }
-
-     public function AccederEvent($id)
+class PaticipantController extends Controller
 {
-    $event = Event::selectEventById($id); 
-    if (!$event) {
-        throw new \Exception('Event not found', 404);
+
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    $this->render('Participant/event_detail', ['event' => $event ,'id' => $id]);
-
-}
-public function faireUneReservation($id_user,$id_event,$ticket_type,$quantity,$total_price,){
-
-}
-
-
+    public function  findAllEvent()
+    {
+        $events = Event::findAllEvent();
+        return $events;
+    }
+    public function accederEventReserver() {}
+    public function Events()
+    {
+        $events = $this->findAllEvent();
+        $this->render('Participant/events', ['events' => $events]);
     }
 
-?>
+    public function getPaginationEvent($page)
+    {
+        $limit = 4;
+        if (!is_numeric($page) || $page < 1) {
+            $page = 1;
+        }
+        $offset = ($page - 1) * $limit;
+        $events = Event::getPaginationEvent($limit, $offset);
+        $totalEvents = count($this->findAllEvent());
+        $totalPages = ceil($totalEvents / $limit);
+        return json_encode([
+            'events' => $events,
+            'currentPage' => (int)$page,
+            'totalPages' => $totalPages,
+            'totalEvents' => $totalEvents
+        ]);
+    }
+
+    public function EventsPagination($page)
+    {
+        $events = $this->getPaginationEvent($page);
+        $page = $events['currentPage'];
+        $totalPages = $events['totalEvents'];
+        $totalEvents = $events['totalEvents'];
+        var_dump($page);
+        if (!$events['events']) {
+            throw new \Exception('Event not found', 404);
+        }
+        $this->render('Participant/events', ['events' => $events['events'], 'page' => $page, 'totalEvents' => $totalPages, 'totalEvents' => $totalEvents]);
+    }
+
+    public function AccederEvent($id)
+    {
+        $event = Event::selectEventById($id);
+        if (!$event) {
+            throw new \Exception('Event not found', 404);
+        }
+
+        $this->render('Participant/event_detail', ['event' => $event, 'id' => $id]);
+    }
+    public function faireUneReservation($id_user, $id_event, $ticket_type, $quantity, $total_price,) {}
+}
